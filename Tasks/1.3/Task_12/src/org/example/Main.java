9bf4873eafeba5ef46fcbba708987d7bb2901b9d
Andrey <112519966+ahahaharu@users.noreply.github.com>
2024-04-07 23:16:51 +0300

    task 12 fixed

--- Tasks/1.3/Task_12/src/org/example/Main.java
+++ Tasks/1.3/Task_12/src/org/example/Main.java
@@ -1,18 +1,22 @@
+package org.example;
+
 import java.util.LinkedList;
 import java.util.Scanner;
+import java.util.logging.Logger;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        Logger logger = Logger.getLogger(Main.class.getName());
 
         LinkedList<String> queue = new LinkedList<>();
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Введите k:");
+        logger.info("Введите k:");
         int k = scanner.nextInt();
 
-        System.out.println("Введите элементы очереди (ctrl + D, чтобы закончить):");
+        logger.info("Введите элементы очереди (ctrl + D, чтобы закончить):");
         while (scanner.hasNextLine()) {
             queue.add(scanner.nextLine());
             if (queue.size() > k) {
@@ -21,9 +25,9 @@ public class Main {
         }
 
         if (queue.size() < k) {
-            System.out.println("Количество строк меньше, чем k");
+            logger.info("Количество строк меньше, чем k");
         } else {
-            System.out.println(queue.peekFirst());
+            logger.info(queue.peekFirst());
         }
     }
 }
