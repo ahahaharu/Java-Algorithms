ab950308d679adfb0f0a2a60c91206e526e80be8
Andrey <112519966+ahahaharu@users.noreply.github.com>
2024-04-08 23:10:44 +0300

    task 15 fixed

--- Tasks/1.3/Task_15/src/org/example/Main.java
+++ Tasks/1.3/Task_15/src/org/example/Main.java
@@ -1,29 +1,42 @@
 package org.example;
 
 import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        LinkedList<String> queue = new LinkedList<>();
+        Queue<String> queue = new LinkedList<>();
         Scanner scanner = new Scanner(System.in);
+        int k;
 
-        System.out.println("Введите k:");
-        int k = scanner.nextInt();
+        while (true) {
+            System.out.println("Введите число k:");
+            while (!scanner.hasNextInt()) {
+                System.out.println("Это не число. Пожалуйста, введите число:");
+                scanner.next();
+            }
+            k = scanner.nextInt();
+            if (k < 0) {
+                System.out.println("Число не может быть меньше 0. Пожалуйста, введите число больше или равно 0:");
+            } else {
+                break;
+            }
+        }
 
         System.out.println("Введите элементы очереди (ctrl + D, чтобы закончить):");
         while (scanner.hasNextLine()) {
             queue.add(scanner.nextLine());
             if (queue.size() > k) {
-                queue.removeFirst();
+                queue.remove();
             }
         }
 
         if (queue.size() < k) {
             System.out.println("Количество строк меньше, чем k");
         } else {
-            System.out.println(queue.peekFirst());
+            System.out.println(queue.peek());
         }
     }
 }
