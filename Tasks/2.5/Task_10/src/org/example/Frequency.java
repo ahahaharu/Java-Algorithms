3d92a03ab240a4c0dddc01075ab68460ca2c6951
Andrey <112519966+ahahaharu@users.noreply.github.com>
2024-04-14 00:06:27 +0300

    task 10 done

---
+++ Tasks/2.5/Task_10/src/org/example/Frequency.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.util.*;
+public class Frequency {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        HashMap<String, Integer> frequencyMap = new HashMap<>();
+
+        System.out.println("Введдите строки (ctrl + D, чтобы закончить):");
+
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            frequencyMap.put(line, frequencyMap.getOrDefault(line, 0) + 1);
+        }
+
+        ArrayList<HashMap.Entry<String, Integer>> list = new ArrayList<>(frequencyMap.entrySet());
+        list.sort(HashMap.Entry.<String, Integer>comparingByValue().reversed());
+
+        for (Map.Entry<String, Integer> entry : list) {
+            System.out.println("Строка: " + entry.getKey() + ", Частота: " + entry.getValue());
+        }
+    }
+}
