345bf03c43af916c709f31f22f160f45f55cf304
Andrey <112519966+ahahaharu@users.noreply.github.com>
2024-05-01 15:34:47 +0300

    task 40 done

---
+++ Tasks/3.3/Task_40/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        RedBlackBST<Integer, Integer> bst = new RedBlackBST<>();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите число для добавления в дерево (или 'ctrl+D' для выхода):");
+
+        while (scanner.hasNextLine()) {
+            String input = scanner.nextLine();
+
+            try {
+                int key = Integer.parseInt(input);
+                bst.put(key, key);
+            } catch (NumberFormatException e) {
+                System.out.println("Некорректный ввод. Пожалуйста, введите число.");
+                continue;
+            }
+        }
+
+        System.out.println("Элементы дерева перед удалением минимального элемента:");
+        bst.inOrderTraversal();
+
+        bst.deleteMin();
+
+        System.out.println("Элементы дерева после удаления минимального элемента:");
+        bst.inOrderTraversal();
+    }
+}
