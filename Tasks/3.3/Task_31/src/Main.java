3699e892636cec4f4907a23aa5af02a96025272e
Andrey <112519966+ahahaharu@users.noreply.github.com>
2024-05-01 16:06:32 +0300

    task 31 done

---
+++ Tasks/3.3/Task_31/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        RedBlackBST<Integer, Integer> bst = new RedBlackBST<>();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите число для добавления в дерево (или 'ctrl+D' для выхода):");
+
+        while (scanner.hasNextLine()) {
+            String input = scanner.nextLine();
+
+            try {
+                int key = Integer.parseInt(input);
+                bst.put(key, key);
+            } catch (NumberFormatException e) {
+                System.out.println("Некорректный ввод. Пожалуйста, введите число.");
+                continue;
+            }
+        }
+
+        System.out.println("Красно-черное дерево:");
+        bst.draw();
+    }
+}
