a20f4cdfdfb8f4ea8984ae4451ea0463205ea63a
Andrey <112519966+ahahaharu@users.noreply.github.com>
2024-05-04 21:38:13 +0300

    task 9 done

---
+++ Tasks/5.1/Task_9/src/LSDSortVariableLength.java
@@ -0,0 +1,61 @@
+public class LSDSortVariableLength {
+    public void lsdSort(String[] array) {
+
+        if (array == null || array.length == 0) {
+            return;
+        }
+
+        int alphabetSize = 256; // Extended ASCII characters
+
+        String[] auxArray = new String[array.length];
+        int maxStringLength = getMaxStringLength(array);
+
+        for (int digit = maxStringLength - 1; digit >= 0; digit--) {
+            // Sort by key-indexed counting on digitTh char
+
+            // Compute frequency counts
+            int count[] = new int[alphabetSize + 1];
+            for (int i = 0; i < array.length; i++) {
+                int digitIndex = charAt(array[i], digit);
+                count[digitIndex + 1]++;
+            }
+
+            // Transform counts to indices
+            for (int r = 0; r < alphabetSize; r++) {
+                count[r + 1] += count[r];
+            }
+
+            // Distribute
+            for (int i = 0; i < array.length; i++) {
+                int digitIndex = charAt(array[i], digit);
+                int indexInAuxArray = count[digitIndex]++;
+                auxArray[indexInAuxArray] = array[i];
+            }
+
+            // Copy back
+            for (int i = 0; i < array.length; i++) {
+                array[i] = auxArray[i];
+            }
+        }
+    }
+
+    private int getMaxStringLength(String[] strings) {
+        int maxLength = -1;
+
+        for (String string : strings) {
+            if (string.length() > maxLength) {
+                maxLength = string.length();
+            }
+        }
+        return maxLength;
+    }
+
+    // If digit is non-existent, return 0, which is the smallest value possible
+    private int charAt(String string, int digit) {
+        if (digit < string.length()) {
+            return string.charAt(digit);
+        } else {
+            return 0;
+        }
+    }
+}
